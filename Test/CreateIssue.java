17e0c44ba1b4135ff5d34ff6861492d10fe52c13
rebak16 <rebakbalazs@gmail.com>
2019-09-30 12:53:29 +0200

    readyforcreate

--- Test/CreateIssue.java
+++ Test/CreateIssue.java
@@ -1,14 +1,16 @@
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class Main {
+public class CreateIssue {
 
-    private static WebDriver driver = new ChromeDriver();
+    private WebDriver driver = new ChromeDriver();
 
-    public static void createJetiProject(){
+    @Test
+    public void createJetiProject(){
         driver.manage().window().maximize();
         driver.navigate().to("https://jira.codecool.codecanvas.hu/secure/Dashboard.jspa");
         WebElement createButton = driver.findElement(By.xpath("//*[@id=\"create_link\"]"));
@@ -17,9 +19,5 @@ public class Main {
         dropDownButton.click();
         Select select = new Select(dropDownButton);
         select.selectByVisibleText("Bug");
-    }
-
-    public static void main(String[] args) {
-        createJetiProject();
     }
 }
