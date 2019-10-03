bc8d4897ed52c9be4dd87aa889bfff6e6d754852
RoyB91 <rolonew@gmail.com>
2019-10-03 14:56:29 +0200

    BuildingProject

--- Test/CreateComponentTestWithGlass.java
+++ Test/CreateComponentTestWithGlass.java
@@ -39,7 +39,7 @@ class CreateComponentTestWithGlass {
         driver.findElement(By.xpath("//*[@id=\"content\"]/div[1]/div/div[1]/nav/div/div[2]/ul/li[7]/a/span[1]")).click();
 
 
-        WebElement table = driver.findElement(By.xpath("//*[@id=\"components-table\"]")).findElement(By.className("items"));
+         WebElement table = driver.findElement(By.xpath("//*[@id=\"components-table\"]")).findElement(By.className("items"));
 
 
         String compName = table.findElement(By.xpath("//*[contains(text(), 'Kritikus komponens')]")).getAttribute("innerText");
