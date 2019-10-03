c967bd316e4d83b6f5067d81949d12533614a83f
RoyB91 <rolonew@gmail.com>
2019-10-03 09:39:53 +0200

    BuildingProject

--- src/CreateIssueSrc.java
+++ src/CreateIssueSrc.java
@@ -34,31 +34,4 @@ public class CreateIssueSrc {
         clickCreateButton.click();
     }
 
-    public void selectIssueType(WebDriver driver, String projectName, String issueType) {
-        Actions action = new Actions(driver);
-        WebDriverWait webDriverWait = new WebDriverWait(driver, 15);
-
-        driver.findElement(By.id("create_link")).click();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
-        driver.findElement(By.xpath("//*[@id=\"project-single-select\"]/span")).click();
-
-
-        webDriverWait.until(ExpectedConditions.attributeToBe(driver.findElement(By.xpath("//*[@id=\"project-field\"]")), "aria-expanded", "true"));
-
-        try {
-            WebElement projectN = driver.findElement(By.cssSelector("#coala-project-\\(coala\\)-7 > .aui-list-item-link"));
-            projectN.click();
-
-        } catch (NoSuchElementException e) {
-            action.sendKeys(Keys.ESCAPE).build().perform();
-        }
-
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        WebElement inputField = driver.findElement(By.xpath("//*[@id=\"issuetype-field\"]"));
-        inputField.sendKeys("Bug");
-        action.sendKeys((Keys.TAB));
-
-    }
-
 }
